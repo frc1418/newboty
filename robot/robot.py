77318f52465040980a5d90049d6968cfd44a76e7
JosephCarpenter <carp5@rcn.com>
2019-01-20 11:30:04 -0500

    Fix formatting errors

--- robot/robot.py
+++ robot/robot.py
@@ -8,9 +8,9 @@ import wpilib.drive
 
 
 class Bot(magicbot.MagicRobot):
-     drive: drive.Drive
+    drive: drive.Drive
 
-     def createObjects(self):
+    def createObjects(self):
         # Joysticks
         self.joystick = wpilib.Joystick(0)
 
@@ -18,25 +18,30 @@ class Bot(magicbot.MagicRobot):
         #   Dig | 0/1
         #   2^1 | Left/Right
         #   2^0 | Front/Rear
-        self.lf_motor = wpilib.Victor(0b00) # =>0
-        self.lr_motor = wpilib.Victor(0b01) # =>1
-        self.rf_motor = wpilib.Victor(0b10) # =>2
-        self.rr_motor = wpilib.Victor(0b11) # =>3
+        self.lf_motor = wpilib.Victor(0b00)  # =>0
+        self.lr_motor = wpilib.Victor(0b01)  # =>1
+        self.rf_motor = wpilib.Victor(0b10)  # =>2
+        self.rr_motor = wpilib.Victor(0b11)  # =>3
 
         self.drivetrain = wpilib.drive.DifferentialDrive(wpilib.SpeedControllerGroup(self.lf_motor, self.lr_motor),
-                                                    wpilib.SpeedControllerGroup(self.rf_motor, self.rr_motor))
+                                                         wpilib.SpeedControllerGroup(self.rf_motor, self.rr_motor))
 
     def autonomous(self):
         super().autonomous()
 
-    def disabledPeriodic(self): pass
-    def disabledInit(self): pass
-    def teleopInit(self): pass
+    def disabledPeriodic(self):
+        pass
+
+    def disabledInit(self):
+        pass
+
+    def teleopInit(self):
+        pass
 
     def teleopPeriodic(self):
         # Normal joysticks
-        self.drive.move(-self.joystick.getY(),self.joystick.getX())
+        self.drive.move(-self.joystick.getY(), self.joystick.getX())
 
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
     wpilib.run(Bot)
